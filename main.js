19cda272d236fe442144c5845c52555de9ae4b8d
Samiksha-coder-cloud <78074734+Samiksha-coder-cloud@users.noreply.github.com>
2021-08-26 15:47:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+Webcam.set({
+    width:310,
+    height:300,
+    image_format:"png",
+    png_quality:90,
+
+    constraints: {
+        facingMode:"environment"
+    }
+});
+
+Webcam.attach("#camera")
+
+function capture_Image() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='captured_image' src='" + data_uri + "'>";
+    });
+}
+
+console.log("ml5 Version- ",ml5.version);
+
+classifier = ml5.imageClassifier("MobileNet", modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded")
+}
+
+function identify_Image() {
+    img =  document.getElementById("captured_image");
+    classifier.classify(img , gotResult);
+}
+
+function gotResult(error , results) {
+    if (error) {
+        console.error(error); 
+    }
+    else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+}
